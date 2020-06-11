ee2b55de429b5d31c06ce6da20adedc0ee8bff3f
broodjeaap <davidzessen@gmail.com>
2020-06-11 21:18:07 +0200

    Moved MonthValuesPage class to it's own file

---
+++ lib/MonthValuesPage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MonthValuesPage extends StatelessWidget {
+  final Map<String, int> monthValues = {
+    "January": 0,
+    "February": 3,
+    "March": 3,
+    "April": 6,
+    "May": 1,
+    "June": 4,
+    "July": 6,
+    "August": 2,
+    "September": 5,
+    "October": 0,
+    "November": 3,
+    "December": 5,
+  };
+
+  final monthTableRowTextStyle = TextStyle(fontSize: 35);
+
+  TableRow getMonthTableRow(MapEntry<String, int> month) {
+    return TableRow(
+        children: [
+          TableCell(
+              child: Text(
+                  month.key,
+                  style: monthTableRowTextStyle,
+                  textAlign: TextAlign.right
+              )
+          ),
+          TableCell(
+              child: Text(
+                  month.value.toString(),
+                  style: monthTableRowTextStyle,
+                  textAlign: TextAlign.center
+              )
+          )
+        ]
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Month Values")
+        ),
+        body: Center(
+            child: Table(
+                children: monthValues.entries.map(getMonthTableRow).toList()
+            )
+        )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'MonthValuesPage.dart';
+
 void main() {
   runApp(OhThatsA());
 }
@@ -15,56 +17,4 @@ class OhThatsA extends StatelessWidget {
   }
 }
 
-class MonthValuesPage extends StatelessWidget {
-  final Map<String, int> monthValues = {
-    "January": 0,
-    "February": 3,
-    "March": 3,
-    "April": 6,
-    "May": 1,
-    "June": 4,
-    "July": 6,
-    "August": 2,
-    "September": 5,
-    "October": 0,
-    "November": 3,
-    "December": 5,
-  };
 
-  final monthTableRowTextStyle = TextStyle(fontSize: 35);
-
-  TableRow getMonthTableRow(MapEntry<String, int> month) {
-    return TableRow(
-        children: [
-          TableCell(
-              child: Text(
-                  month.key,
-                  style: monthTableRowTextStyle,
-                  textAlign: TextAlign.right
-              )
-          ),
-          TableCell(
-              child: Text(
-                  month.value.toString(),
-                  style: monthTableRowTextStyle,
-                  textAlign: TextAlign.center
-              )
-          )
-        ]
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
-              title: Text("Month Values")
-          ),
-          body: Center(
-            child: Table(
-              children: monthValues.entries.map(getMonthTableRow).toList()
-            )
-          )
-      );
-  }
-}
