606326d9b8134e638d28cf473e8916e33473ccb5
broodjeaap <davidzessen@gmail.com>
2020-06-13 14:38:48 +0200

    Added drone CI yml

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,5 +6,14 @@ void main() {
     test("1897", () {
       expect(DayCalculator.getYearValue(1897), equals(2));
     });
+    test("1954", () {
+      expect(DayCalculator.getYearValue(1954), equals(4));
+    });
+    test("2013", () {
+      expect(DayCalculator.getYearValue(2013), equals(2));
+    });
+    test("2132", () {
+      expect(DayCalculator.getYearValue(2132), equals(5));
+    });
   });
 }
