88ddf6f3a858ab9c8d86af1c29df17e65cb4ddde
BroodjeAap <davidzessen@gmail.com>
2020-09-27 14:05:42 +0200

    Added getMonthValue function with tests

--- lib/DayCalculator.dart
+++ lib/DayCalculator.dart
@@ -7,4 +7,46 @@ class DayCalculator {
     var yy = year % 100;
     return (yy + (yy / 4).floor()) % 7;
   }
+
+  static int getMonthValue(int month){
+    // Returns the month value for input 0-11
+    switch(month){
+      case 0:{
+        return 0;
+      }
+      case 1:{
+        return 3;
+      }
+      case 2:{
+        return 3;
+      }
+      case 3:{
+        return 6;
+      }
+      case 4:{
+        return 1;
+      }
+      case 5:{
+        return 4;
+      }
+      case 6:{
+        return 6;
+      }
+      case 7:{
+        return 2;
+      }
+      case 8:{
+        return 5;
+      }
+      case 9:{
+        return 0;
+      }
+      case 10:{
+        return 3;
+      }
+      default:{
+        return 5;
+      }
+    }
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,4 +16,42 @@ void main() {
       expect(DayCalculator.getYearValue(2132), equals(5));
     });
   });
+  group("DayCalculator getMonthValue()", () {
+    test("January", () {
+      expect(DayCalculator.getMonthValue(0), equals(0));
+    });
+    test("February", () {
+      expect(DayCalculator.getMonthValue(1), equals(3));
+    });
+    test("March", () {
+      expect(DayCalculator.getMonthValue(2), equals(3));
+    });
+    test("April", () {
+      expect(DayCalculator.getMonthValue(3), equals(6));
+    });
+    test("May", () {
+      expect(DayCalculator.getMonthValue(4), equals(1));
+    });
+    test("June", () {
+      expect(DayCalculator.getMonthValue(5), equals(4));
+    });
+    test("July", () {
+      expect(DayCalculator.getMonthValue(6), equals(6));
+    });
+    test("August", () {
+      expect(DayCalculator.getMonthValue(7), equals(2));
+    });
+    test("September", () {
+      expect(DayCalculator.getMonthValue(8), equals(5));
+    });
+    test("October", () {
+      expect(DayCalculator.getMonthValue(9), equals(0));
+    });
+    test("November", () {
+      expect(DayCalculator.getMonthValue(10), equals(3));
+    });
+    test("December", () {
+      expect(DayCalculator.getMonthValue(11), equals(5));
+    });
+  });
 }
